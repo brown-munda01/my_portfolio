f2b810610191cac69abb669b300853ad1bb0fdef
brown-munda01 <142725935+brown-munda01@users.noreply.github.com>
2023-08-23 23:35:19 +0530

    build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
+python3.9 manage.py collectstatic
 
 # $ echo yes | python manage.py collectstatic
 
